c886792c5dc8b27ea56f1ccaeb02550ba9199f89
KennethLangedal <kenneth.langedal@hotmail.com>
2025-12-07 09:56:15 +0100

    Fixed vieclus to not fail on extra newline at the end of the file

--- external/vieclus/run.sh
+++ external/vieclus/run.sh
@@ -11,13 +11,13 @@ TIMEOUT=$3
 THREADS=$4
 BASENAME=$(basename "$INPUT_FILE" .graph)
 
-# Make sure we are in the script's directory, so we can find the executables.
-cd "$(dirname "$0")"
-
 # If the input file path is relative, make it absolute.
 if [[ "$INPUT_FILE" != /* ]]; then
     INPUT_FILE="$(pwd)/$INPUT_FILE"
 fi
+
+# Make sure we are in the script's directory, so we can find the executables.
+cd "$(dirname "$0")"
 
 for i in $(seq 1 $K);
 do
@@ -30,24 +30,10 @@ do
 
     if [ -f $LABEL_FILE ]; then
         EVAL_OUT=$(../../EVAL "$INPUT_FILE" "$BASENAME"_clustering.txt "$LABEL_FILE")
-        EVAL_MOD=$(echo "$EVAL_OUT" | awk -F',' '{print $4}')
-        EVAL_N=$(echo "$EVAL_OUT" | awk -F',' '{print $5}')
-        EVAL_CC=$(echo "$EVAL_OUT" | awk -F',' '{print $6}')
-        EVAL_F1=$(echo "$EVAL_OUT" | awk -F',' '{print $7}')
-        EVAL_AC=$(echo "$EVAL_OUT" | awk -F',' '{print $8}')
-        EVAL_TP=$(echo "$EVAL_OUT" | awk -F',' '{print $9}')
-        EVAL_FP=$(echo "$EVAL_OUT" | awk -F',' '{print $10}')
-        EVAL_TN=$(echo "$EVAL_OUT" | awk -F',' '{print $11}')
-        EVAL_FN=$(echo "$EVAL_OUT" | awk -F',' '{print $12}')
-            
-        echo ",$TIME,$EVAL_MOD,$EVAL_N,$EVAL_CC,$EVAL_F1,$EVAL_AC,$EVAL_TP,$EVAL_FP,$EVAL_TN,$EVAL_FN"
+        echo ",$TIME,""$EVAL_OUT"
     else
         EVAL_OUT=$(../../EVAL "$INPUT_FILE" "$BASENAME"_clustering.txt)
-        EVAL_MOD=$(echo "$EVAL_OUT" | awk -F',' '{print $4}')
-        EVAL_N=$(echo "$EVAL_OUT" | awk -F',' '{print $5}')
-        EVAL_CC=$(echo "$EVAL_OUT" | awk -F',' '{print $6}')
-            
-        echo ",$TIME,$EVAL_MOD,$EVAL_N,$EVAL_CC"
+        echo ",$TIME,""$EVAL_OUT"
     fi
 
     rm -rf "$BASENAME"_vieclus_time_mem.txt
