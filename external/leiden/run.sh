e3299b8282a4aab0bf30c97802370bb9f6e82118
KennethLangedal <kenneth.langedal@hotmail.com>
2025-12-07 10:00:18 +0100

    Updated leiden script

--- external/leiden/run.sh
+++ external/leiden/run.sh
@@ -10,13 +10,13 @@ K=$2
 TIMEOUT=$3
 BASENAME=$(basename "$INPUT_FILE" .graph)
 
-# Make sure we are in the script's directory, so we can find the executables.
-cd "$(dirname "$0")"
-
 # If the input file path is relative, make it absolute.
 if [[ "$INPUT_FILE" != /* ]]; then
     INPUT_FILE="$(pwd)/$INPUT_FILE"
 fi
+
+# Make sure we are in the script's directory, so we can find the executables.
+cd "$(dirname "$0")"
 
 source .venv/bin/activate
 
@@ -39,31 +39,13 @@ do
 
         if [ -f $LABEL_FILE ]; then
             EVAL_OUT=$(../../EVAL "$INPUT_FILE" "$FILE" "$LABEL_FILE")
-            EVAL_MOD=$(echo "$EVAL_OUT" | awk -F',' '{print $4}')
-            EVAL_N=$(echo "$EVAL_OUT" | awk -F',' '{print $5}')
-            EVAL_CC=$(echo "$EVAL_OUT" | awk -F',' '{print $6}')
-            EVAL_F1=$(echo "$EVAL_OUT" | awk -F',' '{print $7}')
-            EVAL_AC=$(echo "$EVAL_OUT" | awk -F',' '{print $8}')
-            EVAL_TP=$(echo "$EVAL_OUT" | awk -F',' '{print $9}')
-            EVAL_FP=$(echo "$EVAL_OUT" | awk -F',' '{print $10}')
-            EVAL_TN=$(echo "$EVAL_OUT" | awk -F',' '{print $11}')
-            EVAL_FN=$(echo "$EVAL_OUT" | awk -F',' '{print $12}')
-            
-            echo ",$TIME,$MOD,$EVAL_MOD,$EVAL_N,$EVAL_CC,$EVAL_F1,$EVAL_AC,$EVAL_TP,$EVAL_FP,$EVAL_TN,$EVAL_FN"
+            echo ",$TIME,""$EVAL_OUT"
         else
             EVAL_OUT=$(../../EVAL "$INPUT_FILE" "$FILE")
-            EVAL_MOD=$(echo "$EVAL_OUT" | awk -F',' '{print $4}')
-            EVAL_N=$(echo "$EVAL_OUT" | awk -F',' '{print $5}')
-            EVAL_CC=$(echo "$EVAL_OUT" | awk -F',' '{print $6}')
-            
-            echo ",$TIME,$MOD,$EVAL_MOD,$EVAL_N,$EVAL_CC"
+            echo ",$TIME,""$EVAL_OUT"
         fi
     else
-        if [ -f $LABEL_FILE ]; then
-            echo ",tle,tle,tle,tle,tle,tle,tle,tle,tle,tle,tle"
-        else
-            echo ",tle,tle,tle,tle,tle"
-        fi
+        echo ""
     fi
 done
 
