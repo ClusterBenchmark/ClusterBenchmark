5fa0d8c444e2f65ba8e24251980e02b4bccdeed5
KennethLangedal <kenneth.langedal@hotmail.com>
2025-12-15 16:44:55 +0100

    Updated DMoN scripts

--- external/dmon/run.sh
+++ external/dmon/run.sh
@@ -14,9 +14,6 @@ FEATURES=${6:-} # Set to empty if not provided
 
 BASENAME=$(basename "$INPUT_FILE" .graph)
 
-# Make sure we are in the script's directory, so we can find the executables.
-cd "$(dirname "$0")"
-
 # If the input file path is relative, make it absolute.
 if [[ "$INPUT_FILE" != /* ]]; then
     INPUT_FILE="$(pwd)/$INPUT_FILE"
@@ -26,6 +23,9 @@ fi
 if [[ -n "$FEATURES" && "$FEATURES" != /* ]]; then
     FEATURES="$(pwd)/$FEATURES"
 fi
+
+# Make sure we are in the script's directory, so we can find the executables.
+cd "$(dirname "$0")"
 
 cd google-research/
 
@@ -57,35 +57,16 @@ do
     if [ -f $FILE ]; then
         TIME=$(echo "$PYTHON_OUT" | awk -F',' -v var="$((i * 3 - 2))" '{print $var}')
         IT=$(echo "$PYTHON_OUT" | awk -F',' -v var="$((i * 3 - 1))" '{print $var}')
-        MOD=$(echo "$PYTHON_OUT" | awk -F',' -v var="$((i * 3))" '{print $var}')
 
         if [ -f $LABEL_FILE ]; then
             EVAL_OUT=$(../../../EVAL "$INPUT_FILE" "$FILE" "$LABEL_FILE")
-            EVAL_MOD=$(echo "$EVAL_OUT" | awk -F',' '{print $4}')
-            EVAL_N=$(echo "$EVAL_OUT" | awk -F',' '{print $5}')
-            EVAL_CC=$(echo "$EVAL_OUT" | awk -F',' '{print $6}')
-            EVAL_F1=$(echo "$EVAL_OUT" | awk -F',' '{print $7}')
-            EVAL_AC=$(echo "$EVAL_OUT" | awk -F',' '{print $8}')
-            EVAL_TP=$(echo "$EVAL_OUT" | awk -F',' '{print $9}')
-            EVAL_FP=$(echo "$EVAL_OUT" | awk -F',' '{print $10}')
-            EVAL_TN=$(echo "$EVAL_OUT" | awk -F',' '{print $11}')
-            EVAL_FN=$(echo "$EVAL_OUT" | awk -F',' '{print $12}')
-            
-            echo ",$TIME,$IT,$MOD,$EVAL_MOD,$EVAL_N,$EVAL_CC,$EVAL_F1,$EVAL_AC,$EVAL_TP,$EVAL_FP,$EVAL_TN,$EVAL_FN"
+            echo ",$TIME,$IT,""$EVAL_OUT"
         else
             EVAL_OUT=$(../../../EVAL "$INPUT_FILE" "$FILE")
-            EVAL_MOD=$(echo "$EVAL_OUT" | awk -F',' '{print $4}')
-            EVAL_N=$(echo "$EVAL_OUT" | awk -F',' '{print $5}')
-            EVAL_CC=$(echo "$EVAL_OUT" | awk -F',' '{print $6}')
-            
-            echo ",$TIME,$IT,$MOD,$EVAL_MOD,$EVAL_N,$EVAL_CC"
+            echo ",$TIME,$IT,""$EVAL_OUT"
         fi
     else
-        if [ -f $LABEL_FILE ]; then
-            echo ",tle,tle,tle,tle,tle,tle,tle,tle,tle,tle,tle,tle"
-        else
-            echo ",tle,tle,tle,tle,tle,tle"
-        fi
+        echo ""
     fi
 done
 
